9c802fd82fec305834fcc039489ac78b8482ac0a
vaishali walake <59989341+vaishaliwalake@users.noreply.github.com>
2020-04-27 18:14:21 -0400

    Delete sqlConn.py

--- sqlConn.py
+++
@@ -1,8 +0,0 @@
-import pymysql
-
-
-def getDBConnCursor():
-    conn = pymysql.connect(host='127.0.0.1', port=3306, user='root', passwd='123456', db='loan')
-    cursor = conn.cursor()
-    return cursor,conn
-cursor, conn=getDBConnCursor()
